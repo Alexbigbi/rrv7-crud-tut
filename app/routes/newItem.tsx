ee5b3072fc45c8d5f1cf4daae8508a8ef465cde5
Alexbigbi <122051653+Alexbigbi@users.noreply.github.com>
2025-06-14 13:05:23 -0600

    Style newItem page
    Added tailwindcss to the page

--- app/routes/newItem.tsx
+++ app/routes/newItem.tsx
@@ -28,20 +28,26 @@ export async function action({ request }: ActionFunctionArgs) {
 
 export default function NewItem() {
     return (
-        <div>
-            <h2> Create New Item </h2>
-            <Form method="post">
+        <div className="max-w-md mx-auto">
+            <h2 className="text-2xl font-bold mb-4"> Create New Item </h2>
+            <Form method="post" className="space-y-4 bg-white p-4 rounded shadow">
                 <div>
-                    <label> Title </label>
-                    <input type="text" name="title" required />
+                    <label className="block text-gray-700"> Title </label>
+                    <input type="text" 
+                    name="title" 
+                    className="border border-gray-300 rounded px-3 py-2 w-full" 
+                    required />
                 </div>
 
                 <div>
-                    <label> Description </label>
-                    <textarea name="description" required />
+                    <label className="block text-gray-700"> Content </label>
+                    <textarea name="description" 
+                    className="border border-gray-300 rounded px-3 py-2 w-full"
+                    required 
+                    />
                 </div>
 
-                <button type="submit"> Create Item </button>
+                <button type="submit" className="text-white bg-green-600 px-2 py-1 rounded shadow hover:bg-green-500"> Create Item </button>
             </Form>
         </div>
     );
