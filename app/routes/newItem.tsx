353bcb461f96546cf4d1f6b57dc2327299ae9dfc
Alexbigbi <122051653+Alexbigbi@users.noreply.github.com>
2025-06-14 12:11:20 -0600

    Add title and description input to newItem page
    The page now has text input for the title and description of a new
    item. A submit button is now present, but no routing has been added.
    
    Proper routing must be added in future commits.

--- app/routes/newItem.tsx
+++ app/routes/newItem.tsx
@@ -1,3 +1,26 @@
+import { Form } from "react-router";
+
+export async function action() {
+
+}
+
 export default function NewItem() {
-    return <div> New Item Page</div>
+    return (
+        <div>
+            <h2> Create New Item </h2>
+            <Form method="post">
+                <div>
+                    <label> Title </label>
+                    <input type="text" name="title" required />
+                </div>
+
+                <div>
+                    <label> Description </label>
+                    <textarea name="description" required />
+                </div>
+
+                <button type="submit"> Create Item </button>
+            </Form>
+        </div>
+    );
 }
